803736ae75b8c166c704ef76bbe66ef5ae231fe2
Crowdbotics <team@crowdbotics.com>
2024-03-04 22:00:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delta-dental-member-46805",
+  url: "https://delta-dental-member-46805.botics.co",
+  api: "https://delta-dental-member-46805.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
